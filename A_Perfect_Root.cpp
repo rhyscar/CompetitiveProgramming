9c7c562dbb0d52bc4a3687d44ace269cc041d4bc
rhyscar <rhyscarroll22@gmail.com>
2026-03-13 15:20:19 +1100

    Completed A Perfect Root

---
+++ A_Perfect_Root.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+void solve() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cout << i + 1 << " ";
+;   }
+    std::cout << "\n";
+    
+    // std::cout << answer << "\n"; // Use "\n" instead of std::endl for speed
+}
+
+int main() {
+    // Fast I/O: This is crucial for avoiding Time Limit Exceeded (TLE)
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int t = 1;
+    std::cin >> t; // Reads the number of test cases. 
+                   // If a problem doesn't have multiple test cases, just comment this line out.
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
